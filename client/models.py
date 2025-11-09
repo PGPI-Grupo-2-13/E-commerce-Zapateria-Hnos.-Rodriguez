d7c08514a98260826a8b1ddf2dfc805c3b55453c
adrdiavaz <adrdiavaz@alum.us.es>
2025-11-09 17:08:23 +0100

    feat: modelo de datos de cliente

--- client/models.py
+++ client/models.py
@@ -1,19 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
     
 class Cliente(models.Model):
-    nombre = models.CharField(max_length=15)
-    apellidos = models.CharField(max_length=30)
-    email = models.EmailField(unique=True)
-    telefono = models.CharField(max_length=15, blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    telefono = models.CharField(max_length=15, blank=True)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
-    direccion = models.TextField(blank=True, null=True)
-    ciudad = models.CharField(max_length=50, blank=True, null=True)
-    codigo_postal = models.CharField(max_length=10, blank=True, null=True)
-    password = models.CharField(max_length=128)
+    direccion = models.CharField(max_length=50)
+    ciudad = models.CharField(max_length=50)
+    codigo_postal = models.CharField(max_length=10)
 
 
     def __str__(self):
-        return f"{self.nombre} {self.apellidos}"
+        return f"{self.user.first_name} {self.user.last_name}"
     
