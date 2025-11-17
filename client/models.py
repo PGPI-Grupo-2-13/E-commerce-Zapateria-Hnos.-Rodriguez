b22ba3cd29309c8dc0a6a684aa03efeebd2df9a2
MANUEL ZURITA <mzuritafernandez@gmail.com>
2025-11-17 18:08:34 +0100

    fix: Creación de un cliente al registrar un usuario

--- client/models.py
+++ client/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 # Create your models here.
     
 class Cliente(models.Model):
@@ -14,3 +17,12 @@ class Cliente(models.Model):
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
     
+@receiver(post_save, sender=User)
+def crear_cliente(sender, instance, created, **kwargs):
+    if created:
+        Cliente.objects.create(
+            user=instance,
+            direccion='',
+            ciudad='',
+            codigo_postal=''
+        )
